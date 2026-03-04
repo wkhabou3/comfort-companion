73ba32edd256fd2168cbb074c68edb48c046fd1f
wkhabou3 <wkhabou33@gmail.com>
2026-03-04 10:53:14 -0500

    added Arduino project

---
+++ src/game_loop.ino
@@ -0,0 +1,21 @@
+// MicroSD breakout pins
+const int MOSI = 10;
+const int MISO = 11;
+const int SCK = 21;
+const int CS = 22;
+
+// MAX98357A pins
+const int SD = 18;
+const int DIN = 19;
+const int BCLK = 20;
+const int LRC = 23;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
